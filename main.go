ea1e2a5cbab1a3f042f53cdab3b9051deccaf415
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2016-07-19 00:36:18 -0700

    --auto now fixes all names that `go lint` catches

--- main.go
+++ main.go
@@ -18,13 +18,13 @@ import (
 	"go/token"
 	"os"
 	"path/filepath"
-	"regexp"
 	"strings"
+	"sync"
+	"unicode"
 
 	"go4.org/syncutil"
 
 	"github.com/kisielk/gotool"
-	"github.com/serenize/snaker"
 )
 
 var (
@@ -32,6 +32,13 @@ var (
 	to   = flag.String("to", "", "the new name")
 	auto = flag.Bool("auto", false, "automatically change ALL_CAPS to CamelCase")
 )
+
+var changeLog = struct {
+	sync.Mutex
+	m map[[2]string]bool
+}{
+	m: make(map[[2]string]bool),
+}
 
 func main() {
 	flag.Parse()
@@ -54,6 +61,12 @@ func main() {
 		}
 		os.Exit(1)
 	}
+	if len(changeLog.m) > 0 {
+		fmt.Println("Changed:")
+		for k := range changeLog.m {
+			fmt.Printf("\t%s -> %s\n", k[0], k[1])
+		}
+	}
 }
 
 func renameIn(pkgPath string) error {
@@ -66,6 +79,7 @@ func renameIn(pkgPath string) error {
 		return err
 	}
 	var wg syncutil.Group
+
 	for _, path := range pkg.GoFiles {
 		path := filepath.Join(pkg.Dir, path)
 		wg.Go(func() error {
@@ -78,8 +92,11 @@ func renameIn(pkgPath string) error {
 			ast.Inspect(f, func(node ast.Node) bool {
 				if i, ok := node.(*ast.Ident); ok {
 					if *auto {
-						n := autoFix(i.Name)
+						n := lintName(i.Name)
 						if n != i.Name {
+							changeLog.Lock()
+							changeLog.m[[2]string{i.Name, n}] = true
+							changeLog.Unlock()
 							changed = true
 							i.Name = n
 						}
@@ -104,14 +121,111 @@ func renameIn(pkgPath string) error {
 	return wg.Err()
 }
 
-var (
-	allCapsRE   = regexp.MustCompile(`^[A-Z0-9_]+$`)
-	allUndersRE = regexp.MustCompile(`^_+$`)
-)
-
-func autoFix(name string) string {
-	if allCapsRE.MatchString(name) && !allUndersRE.MatchString(name) && strings.Contains(name, "_") {
-		return snaker.SnakeToCamel(strings.ToLower(name))
+// Copied from go lint.
+// lintName returns a different name if it should be different.
+func lintName(name string) (should string) {
+	// Fast path for simple cases: "_" and all lowercase.
+	if name == "_" {
+		return name
 	}
-	return name
+	allLower := true
+	for _, r := range name {
+		if !unicode.IsLower(r) {
+			allLower = false
+			break
+		}
+	}
+	if allLower {
+		return name
+	}
+
+	// Split camelCase at any lower->upper transition, and split on underscores.
+	// Check each word for common initialisms.
+	runes := []rune(name)
+	w, i := 0, 0 // index of start of word, scan
+	for i+1 <= len(runes) {
+		eow := false // whether we hit the end of a word
+		if i+1 == len(runes) {
+			eow = true
+		} else if runes[i+1] == '_' {
+			// underscore; shift the remainder forward over any run of underscores
+			eow = true
+			n := 1
+			for i+n+1 < len(runes) && runes[i+n+1] == '_' {
+				n++
+			}
+
+			// Leave at most one underscore if the underscore is between two digits
+			if i+n+1 < len(runes) && unicode.IsDigit(runes[i]) && unicode.IsDigit(runes[i+n+1]) {
+				n--
+			}
+
+			copy(runes[i+1:], runes[i+n+1:])
+			runes = runes[:len(runes)-n]
+		} else if unicode.IsLower(runes[i]) && !unicode.IsLower(runes[i+1]) {
+			// lower->non-lower
+			eow = true
+		}
+		i++
+		if !eow {
+			continue
+		}
+
+		// [w,i) is a word.
+		word := string(runes[w:i])
+		if u := strings.ToUpper(word); commonInitialisms[u] {
+			// Keep consistent case, which is lowercase only at the start.
+			if w == 0 && unicode.IsLower(runes[w]) {
+				u = strings.ToLower(u)
+			}
+			// All the common initialisms are ASCII,
+			// so we can replace the bytes exactly.
+			copy(runes[w:], []rune(u))
+		} else if w > 0 && strings.ToLower(word) == word {
+			// already all lowercase, and not the first word, so uppercase the first character.
+			runes[w] = unicode.ToUpper(runes[w])
+		}
+		w = i
+	}
+	return string(runes)
+}
+
+// Copied from go lint.
+var commonInitialisms = map[string]bool{
+	"API":   true,
+	"ASCII": true,
+	"CPU":   true,
+	"CSS":   true,
+	"DNS":   true,
+	"EOF":   true,
+	"GUID":  true,
+	"HTML":  true,
+	"HTTP":  true,
+	"HTTPS": true,
+	"ID":    true,
+	"IP":    true,
+	"JSON":  true,
+	"LHS":   true,
+	"QPS":   true,
+	"RAM":   true,
+	"RHS":   true,
+	"RPC":   true,
+	"SLA":   true,
+	"SMTP":  true,
+	"SQL":   true,
+	"SSH":   true,
+	"TCP":   true,
+	"TLS":   true,
+	"TTL":   true,
+	"UDP":   true,
+	"UI":    true,
+	"UID":   true,
+	"UUID":  true,
+	"URI":   true,
+	"URL":   true,
+	"UTF8":  true,
+	"VM":    true,
+	"XML":   true,
+	"XSRF":  true,
+	"XSS":   true,
 }
